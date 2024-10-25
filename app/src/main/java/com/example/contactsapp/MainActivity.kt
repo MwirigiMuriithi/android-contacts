f853c59b29b57a419cbae765308745f9d3d84c72
MwirigiMuriithi <learnerviner@gmail.com>
2024-10-25 11:04:47 +0300

    Added edit and delete contact screens

--- app/src/main/java/com/example/contactsapp/MainActivity.kt
+++ app/src/main/java/com/example/contactsapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Space
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -21,7 +20,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.R
+//import androidx.compose.runtime.R
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -38,11 +37,12 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import coil.compose.rememberAsyncImagePainter
-import com.example.contactsapp.ui.theme.ContactsAppTheme
 import com.example.contactsapp.ui.theme.GreenJC
 import java.io.File
 import java.io.FileOutputStream
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,7 +58,19 @@ class MainActivity : ComponentActivity() {
            val navController = rememberNavController()
             NavHost(navController = navController, startDestination = "contactList") {
                 composable("contactList") { ContactListScreen(viewModel, navController)}
-                composable("addContact") { AddContactScreen(viewModel, navController )
+                composable("addContact") { AddContactScreen(viewModel, navController ) }
+                composable("contactDetail/{contactId}"){ backStackEntry ->
+                    val contactId = backStackEntry.arguments?.getString("contactId")?.toInt()
+                    val contact = viewModel.allContacts.observeAsState(initial = emptyList()).value.find {it.id == contactId}
+                    contact?.let {
+                        ContactDetailScreen(it, viewModel, navController)
+                    }
+                }
+                composable("editContact/{contactId}"){ backStackEntry ->
+                    val contactId = backStackEntry.arguments?.getString("contactId")?.toInt()
+                    val contact = viewModel.allContacts.observeAsState(initial = emptyList()).value.find {it.id == contactId}
+                    contact?.let { EditContactScreen(it, viewModel, navController)
+                    }
                 }
                 
             }
@@ -266,7 +278,264 @@ fun AddContactScreen(viewModel: ContactViewModel, navController: NavController) 
         }
     }
 }
+@Composable
+fun ContactDetailScreen(contact: Contact, viewModel: ContactViewModel, navController: NavController) {
+    val context = LocalContext.current
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Contact Details", fontSize = 18.sp) }, // Corrected title
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) { // Navigate back instead of showing a toast
+                        Icon(painter = painterResource(id = R.drawable.contactdetails), contentDescription = "Back") // Updated icon resource
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = GreenJC,
+                    titleContentColor = Color.White,
+                    navigationIconContentColor = Color.White
+                )
+            )
+        },
+        floatingActionButton = {
+           FloatingActionButton(containerColor = GreenJC, onClick = { navController.navigate("editContact/${contact.id}")
+           }) {
+               Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit Contact")
+           }
+        }
+    ) { paddingValues ->
+        Column (modifier = Modifier
+            .fillMaxWidth()
+            .padding(paddingValues)
+            .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Card(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+                colors = CardDefaults.cardColors(Color.White),
+                shape = RoundedCornerShape(16.dp),
+                elevation = CardDefaults.cardElevation(8.dp)
+            ) {
+                Column (modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                    Image(painter = rememberAsyncImagePainter(contact.image), contentDescription = contact.name,
+                        modifier = Modifier
+                            .size(128.dp)
+                            .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Card (modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    colors = CardDefaults.cardColors(Color.White),
+                    shape = RoundedCornerShape(16.dp),
+                    elevation = CardDefaults.cardElevation(8.dp) )
+                    {
+                        Row (modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text("Name: ", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(contact.name, fontSize = 16.sp)
+                        }
+
+                    }
+                    Card (modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                        colors = CardDefaults.cardColors(Color.White),
+                        shape = RoundedCornerShape(16.dp),
+                        elevation = CardDefaults.cardElevation(8.dp) )
+                    {
+                        Row (modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text("Phone: ", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(contact.phoneNumber, fontSize = 16.sp)
+                        }
+
+                    }
+
+                    Card (modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                        colors = CardDefaults.cardColors(Color.White),
+                        shape = RoundedCornerShape(16.dp),
+                        elevation = CardDefaults.cardElevation(8.dp) )
+                    {
+                        Row (modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text("Email: ", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(contact.email, fontSize = 16.sp)
+                        }
+
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(colors = ButtonDefaults.buttonColors(GreenJC),
+                onClick = { viewModel.deleteContact(contact)
+                    navController.navigate("contactList"){
+                        popUpTo(0)
+                    }
+                }) {
+                Text("Delete Contact")
+            }
+        }
+    }
+}
+
+@Composable
+fun EditContactScreen(contact: Contact, viewModel: ContactViewModel, navController: NavController){
+    val context = LocalContext.current.applicationContext
+
+    var imageUri by remember {
+        mutableStateOf(contact.image)
+    }
+    var name by remember {
+        mutableStateOf(contact.name)
+    }
+    var phoneNumber by remember {
+        mutableStateOf(contact.phoneNumber)
+    }
+    var email by remember {
+        mutableStateOf(contact.email)
+    }
+
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) {
+        uri: Uri? ->
+        uri?.let {  newUri ->
+            val internalPath = copyUriToInternalStorage(context, newUri, "$name.jpg")
+            internalPath?.let {  path -> imageUri = path}
+        }
+        
+    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Edit contact", fontSize = 18.sp) },
+                navigationIcon = {
+                    IconButton(onClick = { Toast.makeText(context, "Edit Contact", Toast.LENGTH_SHORT).show() }) {
+                        Icon(painter = painterResource(id = R.drawable.editContact), contentDescription = "Add Contact")
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = GreenJC,
+                    titleContentColor = Color.White,
+                    navigationIconContentColor = Color.White
+                )
+            )
+        }
+    ) { paddingValues ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(paddingValues)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+
+                Image(
+                    painter = rememberAsyncImagePainter(imageUri),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(128.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Button(onClick = { launcher.launch("image/*") }, colors = ButtonDefaults.buttonColors(GreenJC)) {
+                Text(text = "Choose Image")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    focusedTextColor = Color.Black,
+                    unfocusedTextColor = Color.Black
+                )
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            TextField(
+                value = phoneNumber,
+                onValueChange = { phoneNumber = it },
+                label = { Text("Phone Number") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    focusedTextColor = Color.Black,
+                    unfocusedTextColor = Color.Black
+                )
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    focusedTextColor = Color.Black,
+                    unfocusedTextColor = Color.Black
+                )
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                val updateContact = contact.copy(image = imageUri, name = name, phoneNumber = phoneNumber, email = email)
+                viewModel.updateContact(updateContact)
+                navController.navigate("contactList"){
+                    popUpTo(0)
+                }
+            }, colors = ButtonDefaults.buttonColors(GreenJC)) {
+                Text(text = "Update Contact")
+            }
+        }
+    }
+
+
+}
 fun copyUriToInternalStorage(context: Context, uri:Uri, fileName: String): String? {
     val file = File(context.filesDir, fileName)
     return try {
